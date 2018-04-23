c9e15b45da03ae291eb89209343fe39fcc1f2b45
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# An example of using the ShowFilterPopupCheckedListBox event
+
+
+<p>This example demonstrates how to remove the "(Select All)" item from the column's filter dropdown.</p>
+
+<br/>
+
+
---
+++ VB/ShowFilterPopupCheckedListBox example/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraTreeList
+
+Namespace ShowFilterPopupCheckedListBox_example
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			Me.table1TableAdapter.Fill(Me.treelistdbDataSet.Table1)
+		End Sub
+
+		Private Sub treeList1_ShowFilterPopupCheckedListBox(ByVal sender As Object, ByVal e As FilterPopupCheckedListBoxEventArgs) Handles treeList1.ShowFilterPopupCheckedListBox
+			If e.Column.FieldName <> "Department" Then
+				Return
+			End If
+			' disable "(Select All)" item
+			e.CheckedComboBox.SelectAllItemVisible = False
+		End Sub
+	End Class
+End Namespace
